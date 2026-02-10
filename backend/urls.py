c68934751cf3b8e90c76fbb8a8b013c1b4973853
itskhashman <khashmanomar22@gmail.com>
2026-02-10 09:36:16 +0300

    Code cleaning : remove extra spaces

--- backend/urls.py
+++ backend/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
 from graphene_django.views import GraphQLView
-from django.views.decorators.csrf import csrf_exempt # disable CSRF Attack.
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path("graphql/", csrf_exempt(GraphQLView.as_view(graphiql=True))),
